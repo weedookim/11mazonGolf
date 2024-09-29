4e136a0d305c7fd2bef93526419f1a62fbb17166
HSKim <hosung.kim@sk.com>
2024-09-29 20:25:15 +0900

    pandas

--- fmkor_crawl.py
+++ fmkor_crawl.py
@@ -9,7 +9,7 @@ from datetime import datetime
 # %%
 url = "https://www.fmkorea.com/index.php?mid=other_sports&category=4657164507"
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
 }
 
 # %%
